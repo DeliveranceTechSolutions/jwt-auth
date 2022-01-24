59426f18f08a76dc005ca020407ef2d804048d6b
deliverancetechsolutions <kirklincoln@gmail.com>
2022-01-24 09:57:43 -0800

    sessions

--- sessions/index.js
+++ sessions/index.js
@@ -14,4 +14,27 @@ const dbOptions = {
 }
 const connection = mongoose.createConnection(dbString, dbOptions);
 
-app.use(express.json();)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// initializing collection 'sessions' in mongo instance
+const sessionStore = new MongoStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+});
+
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // Equals 1 day
+    }
+}));
+
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello world (Sessions)</h1>');
+});
+
+app.listen(3000);
